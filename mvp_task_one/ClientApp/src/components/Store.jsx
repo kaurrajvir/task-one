e81c6bc88249f80c3187b328201d1384ea2fcfd6
kaurrajvir <72539709+kaurrajvir@users.noreply.github.com>
2020-10-14 12:15:27 +1300

    Update Store.jsx

--- mvp_task_one/ClientApp/src/components/Store.jsx
+++ mvp_task_one/ClientApp/src/components/Store.jsx
@@ -11,7 +11,7 @@ export class Store extends Component {
         super(props);
         this.state = {        
             data: [],       
-            totalItems: 10,
+            totalItems: 5,
             currentPage: 1,
             sort: {
                 column: null,
